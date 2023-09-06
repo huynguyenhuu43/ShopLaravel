83b42a20b881463f210fcfb0ca6dfb2fc3829ce3
huynguyenhuu43 <huynguyenhuu43@gmail.com>
2023-09-06 14:25:12 +0700

    update vendor business and bank

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -8,6 +8,8 @@ use Hash;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Admin;
 use App\Models\Vendor;
+use App\Models\VendorsBusinessDetail;
+use App\Models\VendorsBankDetail;
 use Illuminate\Support\Facades\Hash as FacadesHash;
 use Image;
 
@@ -132,9 +134,74 @@ class AdminController extends Controller
             }
             $vendorDetails = Vendor::where('id',Auth::guard('admin')->user()->vendor_id)->first()->toArray();
         }else if($slug=="business"){
+             if($request->isMethod('post')){
+                $data = $request ->all();
+                //echo "<pre>"; print_r($data); die;
 
+                $rules = [
+                    'shop_name'=> 'required|regex:/^[\pL\s\-]+$/u',
+                    'shop_city'=> 'required|regex:/^[\pL\s\-]+$/u',
+                    'shop_mobile' => 'required|numeric',
+                    'address_proof' => 'required',
+                ];
+                $customMessages =[
+                    'shop_name.required'=> 'Bạn phải nhập tên !',
+                    'shop_city.required'=> 'Bạn phải nhập tên thành phố !',
+                    'shop_name.regex'=> 'Định dạng tên không đúng !',
+                    'shop_city.regex'=> 'Định dạng không đúng !',
+                    'shop_mobile.required'=> 'Bạn phải nhập số điện thoại !',
+                    'shop_mobile.numeric'=> 'Định dạng số điện thoại không đúng !',
+                ];
+    
+                $this->validate($request,$rules,$customMessages);
+                //upload admin photo
+                if($request->hasFile('address_proof_image')){
+                    $image_tmp = $request->file('address_proof_image');
+                    if($image_tmp->isValid()){
+                        //get image extension
+                        $extension = $image_tmp->getClientOriginalExtension();
+                        $imageName = rand(111,99999).'.'.$extension;
+                        $imagePath = 'admin/images/proofs/'.$imageName;
+                        Image::make($image_tmp)->save($imagePath);
+                    }
+                }else if(!empty($data['current_address_proof'])){
+                    $imageName = $data['current_address_proof'];
+                }else{
+                    $imageName = "";
+                }
+                    //update in vendor table
+                VendorsBusinessDetail::where('vendor_id',Auth::guard('admin')->user()->vendor_id)->update(['shop_name'=>$data['shop_name'],'shop_mobile'=>$data['shop_mobile'],'shop_address'=>$data['shop_address'],'shop_city'=>$data['shop_city'],'shop_state'=>$data['shop_state'],'shop_country'=>$data['shop_country'],'shop_zipcode'=>$data['shop_zipcode'],'business_license_number'=>$data['business_license_number'],'gst_number'=>$data['gst_number'],'pan-number'=>$data['pan-number'],'address_proof'=>$data['address_proof'],'address_proof_image'=>$imageName]);
+                return redirect()->back()->with('success_message','Cập nhật thông tin thành công !');
+            }
+            $vendorDetails = VendorsBusinessDetail::where('vendor_id',Auth::guard('admin')->user()->vendor_id)->first()->toArray();
+            //dd($vendorDetails);
         }else if($slug=="bank"){
+            if($request->isMethod('post')){
+                $data = $request ->all();
+                //echo "<pre>"; print_r($data); die;
 
+                $rules = [
+                    'bank_name'=> 'required|regex:/^[\pL\s\-]+$/u',
+                    'account_holder_name'=> 'required|regex:/^[\pL\s\-]+$/u',
+                    'account_number' => 'required|numeric',
+                    //'bank_ifcs_code'=>'required|numeric',
+                ];
+                $customMessages =[
+                    'bank_name.required'=> 'Bạn phải nhập tên ngân hàng !',
+                    'account_holder_name'=> 'Bạn phải nhập tên tài khoản !',
+                    'bank_name.regex'=> 'Định dạng không đúng !',
+                    'account_holder_name.regex'=> 'Định dạng không đúng !',
+                    'account_number.required'=> 'Bạn phải nhập số tài khoản !',
+                    'account_number.numeric'=> 'Định dạng số tài khoản không đúng !',
+                    'bank_ifcs_code.required'=> 'Bạn phải nhập mã code !',
+                ];
+    
+                $this->validate($request,$rules,$customMessages);
+                    //update in vendor table
+                VendorsBankDetail::where('vendor_id',Auth::guard('admin')->user()->vendor_id)->update(['account_holder_name'=>$data['account_holder_name'],'bank_name'=>$data['bank_name'],'account_number'=>$data['account_number'],'bank_ifsc_code'=>$data['bank_ifsc_code']]);
+                return redirect()->back()->with('success_message','Cập nhật thông tin thành công !');
+            }
+            $vendorDetails = VendorsBankDetail::where('vendor_id',Auth::guard('admin')->user()->vendor_id)->first()->toArray();
         }
         return view('admin.settings.update_vendor_details')->with(compact('slug','vendorDetails'));
     }
--- resources/views/admin/settings/update_vendor_details.blade.php
+++ resources/views/admin/settings/update_vendor_details.blade.php
@@ -111,9 +111,172 @@
             
           </div>
         @elseif($slug=="business")
+        <div class="row">
+            <div class="col-md-6 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Cập nhật thông tin doanh nghiệp</h4>
+                    @if(Session::has('error_message'))
+                      <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong>Lỗi: </strong> {{ Session::get('error_message') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                    @endif
 
+                    @if(Session::has('success_message'))
+                      <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <strong>Thành công: </strong> {{ Session::get('success_message') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                    @endif
+                    @if ($errors->any())
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                
+                @foreach ($errors->all() as $error)
+                              <li>{{ $error }}</li>
+                            @endforeach
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                @endif
+                  <form class="forms-sample" action="{{ url('admin/update-vendor-details/business') }}" method="post" enctype="multipart/form-data" >@csrf
+                    <div class="form-group">
+                      <label>Email</label>
+                      <input  class="form-control" value="{{ Auth::guard('admin')->user()->email }}" readonly="">
+                    </div>
+                    <div class="form-group">
+                      <label for="shop_name">Tên Shop</label>
+                      <input type="text" class="form-control" id="shop_name" placeholder="Nhập tên shop của bạn" name="shop_name" value="{{ $vendorDetails['shop_name'] }}">
+                    </div>
+                    <div class="form-group">
+                      <label for="shop_address">Địa chỉ Shop</label>
+                      <input type="text" class="form-control" id="shop_address" placeholder="Nhập địa chỉ shop của bạn" name="shop_address" value="{{ $vendorDetails['shop_address'] }}">
+                    </div>
+                    <div class="form-group">
+                      <label for="shop_city">Tỉnh/Thành phố</label>
+                      <input type="text" class="form-control" id="shop_city" placeholder="Nhập tỉnh hoặc thành phố" name="shop_city" value="{{ $vendorDetails['shop_city'] }}">
+                    </div>
+                    <div class="form-website
+                      <label for="shop_state">Quận/huyện</label>
+                      <input type="text" class="form-control" id="shop_state" placeholder="Nhập quận/huyện của bạn" name="shop_state" value="{{ $vendorDetails['shop_state'] }}">
+                    </div>
+                    <div class="form-group">
+                      <label for="shop_country">Quốc gia</label>
+                      <input type="text" class="form-control" id="shop_country" placeholder="Nhập quốc gia của bạn" name="shop_country" value="{{ $vendorDetails['shop_country'] }}">
+                    </div>
+                    <div class="form-group">
+                      <label for="shop_zipcode">Mã Zip</label>
+                      <input type="text" class="form-control" id="shop_zipcode" placeholder="Nhập mã vùng" name="shop_zipcode" value="{{ $vendorDetails['shop_zipcode'] }}">
+                    </div>
+                    <div class="form-group">
+                      <label for="shop_mobile">Số điện thoại</label>
+                      <input type="text" class="form-control" id="shop_mobile" placeholder="Nhập số điện thoại" name="shop_mobile" value=" {{ $vendorDetails['shop_mobile'] }} " required="" maxlength="10" minlength="10">
+                    </div>
+                    <div class="form-group">
+                      <label for="business_license_number">Số giấy phép kinh doanh</label>
+                      <input type="text" class="form-control" id="business_license_number" placeholder="Nhập số giấy phép kinh doanh" name="business_license_number" value="{{ $vendorDetails['business_license_number'] }}">
+                    </div>
+                    <div class="form-group">
+                      <label for="gst_number">GTS</label>
+                      <input type="text" class="form-control" id="gst_number" placeholder="Nhập GTS" name="gst_number" value="{{ $vendorDetails['gst_number'] }}">
+                    </div>
+                    <div class="form-group">
+                      <label for="pan-number">PAN</label>
+                      <input type="text" class="form-control" id="pan-number" placeholder="Nhập PAN" name="pan-number" value="{{ $vendorDetails['pan-number'] }}">
+                    </div>
+                    <div class="form-group">
+                      <label for="address_proof">Giấy tờ chứng minh</label>
+                      <select class="form-control" name="address_proof" id="address_proof">
+                        <option value="CCCD" @if($vendorDetails['address_proof']=="CCCD") selected @endif>Căn cước công dân</option>
+                        <option value="SHK" @if($vendorDetails['address_proof']=="SHK") selected @endif>Sổ hộ khẩu</option>
+                        <option value="HC" @if($vendorDetails['address_proof']=="HC") selected @endif>Hộ chiếu</option>
+                        <option value="GPLX" @if($vendorDetails['address_proof']=="GPLX") selected @endif>Giấy phép lái xe</option>
+                      </select>
+                    </div>
+                    <div class="form-group">
+                      <label for="address_proof_image">Hình ảnh giấy tờ chứng minh</label>
+                      <input type="file" class="form-control" id="address_proof_image"  name="address_proof_image">
+                      @if(!empty($vendorDetails['address_proof_image']))
+                      <a target="_blank" href="{{ url('admin/images/proofs/'.$vendorDetails['address_proof_image']) }}">Xem hình</a>
+                      <input type="hidden" name="current_address_proof" value="{{ $vendorDetails['address_proof_image'] }}">
+                      @endif
+                    </div>
+                    <button type="submit" class="btn btn-primary mr-2">Xác nhận</button>
+                    <button class="btn btn-light">Hủy</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+            
+          </div>
         @elseif($slug=="bank")
+        <div class="row">
+            <div class="col-md-6 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Cập nhật thông tin ngân hàng</h4>
+                    @if(Session::has('error_message'))
+                      <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong>Lỗi: </strong> {{ Session::get('error_message') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                    @endif
 
+                    @if(Session::has('success_message'))
+                      <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <strong>Thành công: </strong> {{ Session::get('success_message') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                    @endif
+                    @if ($errors->any())
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                
+                @foreach ($errors->all() as $error)
+                              <li>{{ $error }}</li>
+                            @endforeach
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                @endif
+                  <form class="forms-sample" action="{{ url('admin/update-vendor-details/bank') }}" method="post" enctype="multipart/form-data" >@csrf
+                    <div class="form-group">
+                      <label>Email</label>
+                      <input  class="form-control" value="{{ Auth::guard('admin')->user()->email }}" readonly="">
+                    </div>
+                    <div class="form-group">
+                      <label for="account_holder_name">Tên tài khoản</label>
+                      <input type="text" class="form-control" id="account_holder_name" placeholder="Nhập Tên tài khoản" name="account_holder_name" value="{{ $vendorDetails['account_holder_name'] }}">
+                    </div>
+                    <div class="form-group">
+                      <label for="bank_name">Tên ngân hàng</label>
+                      <input type="text" class="form-control" id="bank_name" placeholder="Nhập Tên ngân hàng" name="bank_name" value="{{ $vendorDetails['bank_name'] }}">
+                    </div>
+                    <div class="form-group">
+                      <label for="account_number">Số tài khoản</label>
+                      <input type="text" class="form-control" id="account_number" placeholder="Nhập Số tài khoản" name="account_number" value="{{ $vendorDetails['account_number'] }}">
+                    </div>
+                    <div class="form-group">
+                      <label for="">Mã Code</label>
+                      <input type="text" class="form-control" id="bank_ifsc_code" placeholder="Nhập Mã Code" name="bank_ifsc_code" value="{{ $vendorDetails['bank_ifsc_code'] }}">
+                    </div>
+                    <button type="submit" class="btn btn-primary mr-2">Xác nhận</button>
+                    <button class="btn btn-light">Hủy</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+            
+          </div>
         @endif
     </div>
     <!-- content-wrapper ends -->
